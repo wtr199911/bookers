85487c5acf6c21b543cb870a23fa75757d235163
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-07-05 04:14:46 +0000

    課題２　途中 css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      @newbooks = Book.all
+      render :index
+    end
   end
 
   def show
@@ -17,6 +21,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @editbook = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "books"
   end
 
   private
