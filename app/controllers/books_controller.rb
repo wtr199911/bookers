e30a99f6003057057e175ecbc18e535668948f2e
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-07-05 13:36:05 +0000

    課題2 修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,8 +27,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @editbook = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
   get 'top' => 'homes#top'
   #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books #only: [:create,:index,:show,:edit,:update]
